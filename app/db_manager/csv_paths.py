4572fbb6b56582819c33811ca9a49b0cce2c1939
larchadrg <lara.combina11@gmail.com>
2024-09-01 00:43:02 -0300

    a

--- app/db_manager/csv_paths.py
+++ app/db_manager/csv_paths.py
@@ -1,11 +1,9 @@
-from os.path import abspath, join 
+from os.path import abspath, join, dirname
 
-CHARACTERS_CSV_PATH = abspath(join('db_manager', 'csv_files', "characters.csv"))
+BASE_DIR = dirname(abspath(__file__))
 
-RANKS_CSV_PATH = abspath(join('db_manager', 'csv_files', 'ranks.csv'))
-
-SIGILS_CSV_PATH = abspath(join('db_manager', 'csv_files', 'sigils.csv'))
-
-ELEMENTS_CSV_PATH = abspath(join('db_manager', 'csv_files', 'elements.csv'))
-
-WARP_SKILLS_CSV_PATH = abspath(join('db_manager', 'csv_files', 'warp_skills.csv'))
+CHARACTERS_CSV_PATH = abspath(join(BASE_DIR,'csv_files','characters.csv'))
+RANKS_CSV_PATH = abspath(join(BASE_DIR,'csv_files','ranks.csv'))
+SIGILS_CSV_PATH = abspath(join(BASE_DIR, 'csv_files','sigils.csv'))
+ELEMENTS_CSV_PATH = abspath(join(BASE_DIR,  'csv_files','elements.csv'))
+WARP_SKILLS_CSV_PATH = abspath(join(BASE_DIR, 'csv_files', 'warp_skills.csv'))
